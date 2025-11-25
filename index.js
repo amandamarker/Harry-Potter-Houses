11e5fecd665c0c99f21fbcd370f7bda36d6cbb54
amandamarker <amandamarker1090@gmail.com>
2025-11-25 12:23:16 -0600

    commit 2

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
-function searchWizards() {
-    console.log('123')
+const wizardsWrapper = document.querySelector('.wizards');
+console.log(wizardsWrapper);
+function searchWizards(event) {
+    console.log(event.target.value)
 }
+
+async function rendorWizards() {
+    const response = await fetch('https://wizard-world-api.herokuapp.com/Wizards')
+    const data =  await response.json()
+    const wizardsArr = data.Search
+    wizardsWrapper.innerHTML = wizardsArr.map((wizards) => {
+        return `
+        div class="wizards">
+        `
+    })
+}
+
+rendorWizards()
